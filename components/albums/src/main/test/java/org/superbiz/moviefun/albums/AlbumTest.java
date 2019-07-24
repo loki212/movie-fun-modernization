65f0802a542f3f145e1391f6b4ad42fd1161ccaa
loki212 <lokeshprasadjagatha@gmail.com>
2019-07-24 11:35:20 +0530

    multiple jars

--- components/albums/src/main/test/java/org/superbiz/moviefun/albums/AlbumTest.java
+++ components/albums/src/main/test/java/org/superbiz/moviefun/albums/AlbumTest.java
@@ -1,6 +1,7 @@
 package org.superbiz.moviefun.albums;
 
 import org.junit.Test;
+import org.superbiz.moviefun.Album;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
